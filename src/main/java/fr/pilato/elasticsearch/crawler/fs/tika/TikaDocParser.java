dadoonet/fscrawler

commit 1a66966991bb360398481f1aaa7a8d95206773dd

Add support for more metadata OOTB
We add support for:

|         Field        |                Description                  |                    Example                  |                                                          Javadoc                                               |
|----------------------|---------------------------------------------|---------------------------------------------|----------------------------------------------------------------------------------------------------------------|
| `meta.format`        | Format of the media                         | `"application/pdf; version=1.6"`            |[FORMAT](https://tika.apache.org/1.16/api/org/apache/tika/metadata/TikaCoreProperties.html#FORMAT)              |
| `meta.identifier`    | URL/DOI/ISBN for example                    | `"FOOBAR"`                                  |[IDENTIFIER](https://tika.apache.org/1.16/api/org/apache/tika/metadata/TikaCoreProperties.html#IDENTIFIER)      |
| `meta.contributor`   | Contributor                                 | `"foo bar"`                                 |[CONTRIBUTOR](https://tika.apache.org/1.16/api/org/apache/tika/metadata/TikaCoreProperties.html#CONTRIBUTOR)    |
| `meta.coverage`      | Coverage                                    | `"FOOBAR"`                                  |[COVERAGE](https://tika.apache.org/1.16/api/org/apache/tika/metadata/TikaCoreProperties.html#COVERAGE)          |
| `meta.modifier`      | Last author                                 | `"David Pilato"`                            |[MODIFIER](https://tika.apache.org/1.16/api/org/apache/tika/metadata/TikaCoreProperties.html#MODIFIER)          |
| `meta.creator_tool`  | Tool used to create the resource            | `"HTML2PDF - TCPDF"`                        |[CREATOR_TOOL](https://tika.apache.org/1.16/api/org/apache/tika/metadata/TikaCoreProperties.html#CREATOR_TOOL)  |

src/main/java/fr/pilato/elasticsearch/crawler/fs/tika/TikaDocParser.java
@@ -21,11 +21,13 @@ package fr.pilato.elasticsearch.crawler.fs.tika;
 
 import fr.pilato.elasticsearch.crawler.fs.meta.doc.Doc;
 import fr.pilato.elasticsearch.crawler.fs.meta.settings.FsSettings;
+import fr.pilato.elasticsearch.crawler.fs.util.FsCrawlerUtil;
 import org.apache.commons.io.input.TeeInputStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tika.language.detect.LanguageResult;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.Property;
 import org.apache.tika.metadata.TikaCoreProperties;
 
 import java.io.ByteArrayOutputStream;
@@ -33,16 +35,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static fr.pilato.elasticsearch.crawler.fs.tika.TikaInstance.extractText;
 import static fr.pilato.elasticsearch.crawler.fs.tika.TikaInstance.langDetector;
-import static fr.pilato.elasticsearch.crawler.fs.util.FsCrawlerUtil.localDateTimeToDate;
 
 /**
  * Parse a binary document and generate a FSCrawler Doc
@@ -121,20 +121,47 @@ public class TikaDocParser {
         }
         // File
 
-        // Meta
-        doc.getMeta().setAuthor(metadata.get(TikaCoreProperties.CREATOR));
-        doc.getMeta().setTitle(metadata.get(TikaCoreProperties.TITLE));
-        String sDate = metadata.get(TikaCoreProperties.MODIFIED);
-        if (sDate != null) {
-            try {
-                LocalDateTime date = LocalDateTime.parse(sDate, DateTimeFormatter.ISO_DATE_TIME);
-                // We assume that local documents have the same date as the system date
-                doc.getMeta().setDate(localDateTimeToDate(date));
-            } catch (DateTimeParseException e) {
-                logger.warn("Can not parse date [{}] for [{}]. Skipping date field...", sDate, filename);
+        // Standard Meta
+        setMeta(filename, metadata, TikaCoreProperties.CREATOR, doc.getMeta()::setAuthor, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.TITLE, doc.getMeta()::setTitle, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.MODIFIED, doc.getMeta()::setDate, FsCrawlerUtil::localDateTimeToDate);
+        setMeta(filename, metadata, TikaCoreProperties.KEYWORDS, doc.getMeta()::setKeywords, TikaDocParser::commaDelimitedListToStringArray);
+        setMeta(filename, metadata, TikaCoreProperties.FORMAT, doc.getMeta()::setFormat, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.IDENTIFIER, doc.getMeta()::setIdentifier, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.CONTRIBUTOR, doc.getMeta()::setContributor, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.COVERAGE, doc.getMeta()::setCoverage, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.MODIFIER, doc.getMeta()::setModifier, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.CREATOR_TOOL, doc.getMeta()::setCreatorTool, Function.identity());
+        String finalParsedContent = parsedContent;
+        setMeta(filename, metadata, TikaCoreProperties.LANGUAGE, doc.getMeta()::setLanguage, (lang) -> {
+            if (lang != null) {
+                return lang;
+            } else if (fsSettings.getFs().isLangDetect() && finalParsedContent != null) {
+                List<LanguageResult> languages = langDetector().detectAll(finalParsedContent);
+                if (!languages.isEmpty()) {
+                    LanguageResult language = languages.get(0);
+                    logger.trace("Main detected language: [{}]", language);
+                    return language.getLanguage();
+                }
             }
-        }
-        doc.getMeta().setKeywords(commaDelimitedListToStringArray(metadata.get(TikaCoreProperties.KEYWORDS)));
+            return null;
+        });
+        setMeta(filename, metadata, TikaCoreProperties.PUBLISHER, doc.getMeta()::setPublisher, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.RELATION, doc.getMeta()::setRelation, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.RIGHTS, doc.getMeta()::setRights, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.SOURCE, doc.getMeta()::setSource, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.TYPE, doc.getMeta()::setType, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.DESCRIPTION, doc.getMeta()::setDescription, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.CREATED, doc.getMeta()::setCreated, FsCrawlerUtil::localDateTimeToDate);
+        setMeta(filename, metadata, TikaCoreProperties.PRINT_DATE, doc.getMeta()::setPrintDate, FsCrawlerUtil::localDateTimeToDate);
+        setMeta(filename, metadata, TikaCoreProperties.METADATA_DATE, doc.getMeta()::setMetadataDate, FsCrawlerUtil::localDateTimeToDate);
+        setMeta(filename, metadata, TikaCoreProperties.LATITUDE, doc.getMeta()::setLatitude, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.LONGITUDE, doc.getMeta()::setLongitude, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.ALTITUDE, doc.getMeta()::setAltitude, Function.identity());
+        setMeta(filename, metadata, TikaCoreProperties.RATING, doc.getMeta()::setRating, Integer::parseInt);
+        setMeta(filename, metadata, TikaCoreProperties.COMMENTS, doc.getMeta()::setComments, Function.identity());
+
+        // Add support for more OOTB standard metadata
 
         if (fsSettings.getFs().isRawMetadata()) {
             logger.trace("Listing all available metadata:");
@@ -146,15 +173,6 @@ public class TikaDocParser {
 
                 // We need to remove dots in field names if any. See https://github.com/dadoonet/fscrawler/issues/256
                 doc.getMeta().addRaw(metadataName.replaceAll("\\.", ":"), value);
-            }
-        }
-
-        if (fsSettings.getFs().isLangDetect() && parsedContent != null) {
-            List<LanguageResult> languages = langDetector().detectAll(parsedContent);
-            if (!languages.isEmpty()) {
-                LanguageResult language = languages.get(0);
-                logger.trace("Main detected language: [{}]", language);
-                doc.getMeta().setLanguage(language.getLanguage());
             }
         }
         // Meta
@@ -169,6 +187,15 @@ public class TikaDocParser {
         }
         logger.trace("End document generation");
         // End of our document
+    }
+
+    private static <T> void setMeta(String filename, Metadata metadata, Property property, Consumer<T> setter, Function<String,T> transformer) {
+        String sMeta = metadata.get(property);
+            try {
+                setter.accept(transformer.apply(sMeta));
+            } catch (Exception e) {
+                logger.warn("Can not parse meta [{}] for [{}]. Skipping [{}] field...", sMeta, filename, property.getName());
+            }
     }
 
     private static List<String> commaDelimitedListToStringArray(String str) {

src/main/java/fr/pilato/elasticsearch/crawler/fs/meta/doc/Attributes.java
@@ -19,8 +19,6 @@
 
 package fr.pilato.elasticsearch.crawler.fs.meta.doc;
 
-import java.util.Objects;
-
 /**
  * Represents additional file attributes.
  */
@@ -51,28 +49,5 @@ public class Attributes {
 
     public void setGroup(String group) {
         this.group = group;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        final Attributes that = (Attributes) o;
-        return Objects.equals(owner, that.owner) &&
-                Objects.equals(group, that.group);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(owner, group);
-    }
-
-    @Override public String toString() {
-        String sb = "Attributes{" + "owner='" + owner + '\'' +
-                ", group='" + group + '\'' +
-                '}';
-        return sb;
     }
 }

src/main/java/fr/pilato/elasticsearch/crawler/fs/meta/doc/Doc.java
@@ -107,30 +107,4 @@ public class Doc {
     public void setAttributes(Attributes attributes) {
         this.attributes = attributes;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Doc doc = (Doc) o;
-
-        if (content != null ? !content.equals(doc.content) : doc.content != null) return false;
-        if (attachment != null ? !attachment.equals(doc.attachment) : doc.attachment != null) return false;
-        if (meta != null ? !meta.equals(doc.meta) : doc.meta != null) return false;
-        if (file != null ? !file.equals(doc.file) : doc.file != null) return false;
-        if (attributes != null ? !attributes.equals(doc.attributes) : doc.attributes != null) return false;
-        return !(path != null ? !path.equals(doc.path) : doc.path != null);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = content != null ? content.hashCode() : 0;
-        result = 31 * result + (attachment != null ? attachment.hashCode() : 0);
-        result = 31 * result + (meta != null ? meta.hashCode() : 0);
-        result = 31 * result + (file != null ? file.hashCode() : 0);
-        result = 31 * result + (path != null ? path.hashCode() : 0);
-        result = 31 * result + (attributes != null ? attributes.hashCode() : 0);
-        return result;
-    }
 }

src/main/java/fr/pilato/elasticsearch/crawler/fs/meta/doc/File.java
@@ -122,37 +122,4 @@ public class File {
     public String getChecksum() {
         return checksum;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        File file = (File) o;
-
-        if (extension != null ? !extension.equals(file.extension) : file.extension != null) return false;
-        if (contentType != null ? !contentType.equals(file.contentType) : file.contentType != null) return false;
-        if (lastModified != null ? !lastModified.equals(file.lastModified) : file.lastModified != null) return false;
-        if (indexingDate != null ? !indexingDate.equals(file.indexingDate) : file.indexingDate != null) return false;
-        if (filesize != null ? !filesize.equals(file.filesize) : file.filesize != null) return false;
-        if (filename != null ? !filename.equals(file.filename) : file.filename != null) return false;
-        if (url != null ? !url.equals(file.url) : file.url != null) return false;
-        if (indexedChars != null ? !indexedChars.equals(file.indexedChars) : file.indexedChars != null) return false;
-        return checksum != null ? checksum.equals(file.checksum) : file.checksum == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = extension != null ? extension.hashCode() : 0;
-        result = 31 * result + (contentType != null ? contentType.hashCode() : 0);
-        result = 31 * result + (lastModified != null ? lastModified.hashCode() : 0);
-        result = 31 * result + (indexingDate != null ? indexingDate.hashCode() : 0);
-        result = 31 * result + (filesize != null ? filesize.hashCode() : 0);
-        result = 31 * result + (filename != null ? filename.hashCode() : 0);
-        result = 31 * result + (url != null ? url.hashCode() : 0);
-        result = 31 * result + (indexedChars != null ? indexedChars.hashCode() : 0);
-        result = 31 * result + (checksum != null ? checksum.hashCode() : 0);
-        return result;
-    }
 }

src/main/java/fr/pilato/elasticsearch/crawler/fs/meta/doc/Meta.java
@@ -38,6 +38,26 @@ public class Meta {
         public static final String DATE = "date";
         public static final String KEYWORDS = "keywords";
         public static final String LANGUAGE = "language";
+        public static final String FORMAT = "format";
+        public static final String IDENTIFIER = "identifier";
+        public static final String CONTRIBUTOR = "contributor";
+        public static final String COVERAGE = "coverage";
+        public static final String MODIFIER = "modifier";
+        public static final String CREATOR_TOOL = "creator_tool";
+        public static final String PUBLISHER = "publisher";
+        public static final String RELATION = "relation";
+        public static final String RIGHTS = "rights";
+        public static final String SOURCE = "source";
+        public static final String TYPE = "type";
+        public static final String DESCRIPTION = "description";
+        public static final String CREATED = "created";
+        public static final String PRINT_DATE = "print_date";
+        public static final String METADATA_DATE = "metadata_date";
+        public static final String LATITUDE = "latitude";
+        public static final String LONGITUDE = "longitude";
+        public static final String ALTITUDE = "altitude";
+        public static final String RATING = "rating";
+        public static final String COMMENTS = "comments";
     }
 
     private String author;
@@ -45,6 +65,26 @@ public class Meta {
     private Date date;
     private List<String> keywords;
     private String language;
+    private String format;
+    private String identifier;
+    private String contributor;
+    private String coverage;
+    private String modifier;
+    private String creatorTool;
+    private String publisher;
+    private String relation;
+    private String rights;
+    private String source;
+    private String type;
+    private String description;
+    private Date created;
+    private Date printDate;
+    private Date metadataDate;
+    private String latitude;
+    private String longitude;
+    private String altitude;
+    private Integer rating;
+    private String comments;
 
     private Map<String, String> raw;
 
@@ -103,26 +143,162 @@ public class Meta {
         this.language = language;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Meta meta = (Meta) o;
-
-        if (author != null ? !author.equals(meta.author) : meta.author != null) return false;
-        if (title != null ? !title.equals(meta.title) : meta.title != null) return false;
-        if (date != null ? !date.equals(meta.date) : meta.date != null) return false;
-        return !(keywords != null ? !keywords.equals(meta.keywords) : meta.keywords != null);
-
+    public String getFormat() {
+        return format;
     }
 
-    @Override
-    public int hashCode() {
-        int result = author != null ? author.hashCode() : 0;
-        result = 31 * result + (title != null ? title.hashCode() : 0);
-        result = 31 * result + (date != null ? date.hashCode() : 0);
-        result = 31 * result + (keywords != null ? keywords.hashCode() : 0);
-        return result;
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getContributor() {
+        return contributor;
+    }
+
+    public void setContributor(String contributor) {
+        this.contributor = contributor;
+    }
+
+    public String getCoverage() {
+        return coverage;
+    }
+
+    public void setCoverage(String coverage) {
+        this.coverage = coverage;
+    }
+
+    public String getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(String modifier) {
+        this.modifier = modifier;
+    }
+
+    public String getCreatorTool() {
+        return creatorTool;
+    }
+
+    public void setCreatorTool(String creatorTool) {
+        this.creatorTool = creatorTool;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getRelation() {
+        return relation;
+    }
+
+    public void setRelation(String relation) {
+        this.relation = relation;
+    }
+
+    public String getRights() {
+        return rights;
+    }
+
+    public void setRights(String rights) {
+        this.rights = rights;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getPrintDate() {
+        return printDate;
+    }
+
+    public void setPrintDate(Date printDate) {
+        this.printDate = printDate;
+    }
+
+    public Date getMetadataDate() {
+        return metadataDate;
+    }
+
+    public void setMetadataDate(Date metadataDate) {
+        this.metadataDate = metadataDate;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(String altitude) {
+        this.altitude = altitude;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
     }
 }

src/main/java/fr/pilato/elasticsearch/crawler/fs/meta/doc/Path.java
@@ -61,25 +61,4 @@ public class Path {
     public void setReal(String real) {
         this.real = real;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Path path = (Path) o;
-
-        if (root != null ? !root.equals(path.root) : path.root != null) return false;
-        if (virtual != null ? !virtual.equals(path.virtual) : path.virtual != null) return false;
-        return !(real != null ? !real.equals(path.real) : path.real != null);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = root != null ? root.hashCode() : 0;
-        result = 31 * result + (virtual != null ? virtual.hashCode() : 0);
-        result = 31 * result + (real != null ? real.hashCode() : 0);
-        return result;
-    }
 }

src/main/java/fr/pilato/elasticsearch/crawler/fs/tika/TikaInstance.java
@@ -133,9 +133,14 @@ public class TikaInstance {
         return handler.toString();
     }
 
-    static LanguageDetector langDetector() throws IOException {
+    static LanguageDetector langDetector() {
         if (detector == null) {
-             detector = getDefaultLanguageDetector().loadModels();
+            try {
+                detector = getDefaultLanguageDetector();
+                detector.loadModels();
+            } catch (IOException e) {
+                logger.warn("Can not load lang detector models", e);
+            }
         }
         return detector;
     }

src/main/java/fr/pilato/elasticsearch/crawler/fs/util/FsCrawlerUtil.java
@@ -48,6 +48,7 @@ import java.nio.file.attribute.PosixFileAttributeView;
 import java.nio.file.attribute.PosixFileAttributes;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.EnumSet;
@@ -216,6 +217,13 @@ public class FsCrawlerUtil extends MetaParser {
 
     public static Date localDateTimeToDate(LocalDateTime ldt) {
         return Date.from(ldt.atZone(TimeZone.getDefault().toZoneId()).toInstant());
+    }
+
+    public static Date localDateTimeToDate(String sDate) {
+        if (sDate == null) {
+            return null;
+        }
+        return localDateTimeToDate(LocalDateTime.parse(sDate, DateTimeFormatter.ISO_DATE_TIME));
     }
 
     public static String getFileExtension(File file) {

commit 1b7974ac00836c43b939d194490bf5eebeb5dcf4

Fix warning messages when extracting rating field
Bug introduced with #423.

src/main/java/fr/pilato/elasticsearch/crawler/fs/tika/TikaDocParser.java
@@ -158,7 +158,7 @@ public class TikaDocParser {
         setMeta(filename, metadata, TikaCoreProperties.LATITUDE, doc.getMeta()::setLatitude, Function.identity());
         setMeta(filename, metadata, TikaCoreProperties.LONGITUDE, doc.getMeta()::setLongitude, Function.identity());
         setMeta(filename, metadata, TikaCoreProperties.ALTITUDE, doc.getMeta()::setAltitude, Function.identity());
-        setMeta(filename, metadata, TikaCoreProperties.RATING, doc.getMeta()::setRating, Integer::parseInt);
+        setMeta(filename, metadata, TikaCoreProperties.RATING, doc.getMeta()::setRating, (value) -> value == null ? null : Integer.parseInt(value));
         setMeta(filename, metadata, TikaCoreProperties.COMMENTS, doc.getMeta()::setComments, Function.identity());
 
         // Add support for more OOTB standard metadata
@@ -191,16 +191,16 @@ public class TikaDocParser {
 
     private static <T> void setMeta(String filename, Metadata metadata, Property property, Consumer<T> setter, Function<String,T> transformer) {
         String sMeta = metadata.get(property);
-            try {
-                setter.accept(transformer.apply(sMeta));
-            } catch (Exception e) {
-                logger.warn("Can not parse meta [{}] for [{}]. Skipping [{}] field...", sMeta, filename, property.getName());
-            }
+        try {
+            setter.accept(transformer.apply(sMeta));
+        } catch (Exception e) {
+            logger.warn("Can not parse meta [{}] for [{}]. Skipping [{}] field...", sMeta, filename, property.getName());
+        }
     }
 
     private static List<String> commaDelimitedListToStringArray(String str) {
         if (str == null) {
-            return new ArrayList<>();
+            return null;
         }
         List<String> result = new ArrayList<>();
         int pos = 0;
@@ -215,6 +215,4 @@ public class TikaDocParser {
         }
         return result;
     }
-
-
 }

src/test/java/fr/pilato/elasticsearch/crawler/fs/test/unit/parser/TikaDocParserTest.java
@@ -102,7 +102,7 @@ public class TikaDocParserTest extends DocParserTestCase {
         // Meta data
         assertThat(doc.getMeta().getAuthor(), is(nullValue()));
         assertThat(doc.getMeta().getDate(), is(localDateTimeToDate(LocalDateTime.of(2016, 9, 19, 14, 29, 37))));
-        assertThat(doc.getMeta().getKeywords(), emptyIterable());
+        assertThat(doc.getMeta().getKeywords(), nullValue());
         assertThat(doc.getMeta().getTitle(), is(nullValue()));
     }
 
@@ -122,7 +122,7 @@ public class TikaDocParserTest extends DocParserTestCase {
         // Meta data
         assertThat(doc.getMeta().getAuthor(), is(nullValue()));
         assertThat(doc.getMeta().getDate(), is(nullValue()));
-        assertThat(doc.getMeta().getKeywords(), emptyIterable());
+        assertThat(doc.getMeta().getKeywords(), nullValue());
         assertThat(doc.getMeta().getTitle(), is(nullValue()));
 
         Map<String, String> raw = doc.getMeta().getRaw();
@@ -288,7 +288,7 @@ public class TikaDocParserTest extends DocParserTestCase {
         // Meta data
         assertThat(doc.getMeta().getAuthor(), is(nullValue()));
         assertThat(doc.getMeta().getDate(), is(nullValue()));
-        assertThat(doc.getMeta().getKeywords(), emptyIterable());
+        assertThat(doc.getMeta().getKeywords(), nullValue());
         assertThat(doc.getMeta().getTitle(), is("Test Tika title"));
 
         Map<String, String> raw = doc.getMeta().getRaw();
@@ -322,7 +322,7 @@ public class TikaDocParserTest extends DocParserTestCase {
         // Meta data
         assertThat(doc.getMeta().getAuthor(), is("David Pilato"));
         assertThat(doc.getMeta().getDate(), is(nullValue()));
-        assertThat(doc.getMeta().getKeywords(), emptyIterable());
+        assertThat(doc.getMeta().getKeywords(), nullValue());
         assertThat(doc.getMeta().getTitle(), is("Test Tika"));
 
         Map<String, String> raw = doc.getMeta().getRaw();
@@ -518,7 +518,7 @@ public class TikaDocParserTest extends DocParserTestCase {
         // Meta data
         assertThat(doc.getMeta().getAuthor(), is(nullValue()));
         assertThat(doc.getMeta().getDate(), is(nullValue()));
-        assertThat(doc.getMeta().getKeywords(), emptyIterable());
+        assertThat(doc.getMeta().getKeywords(), nullValue());
         assertThat(doc.getMeta().getTitle(), is(nullValue()));
 
         Map<String, String> raw = doc.getMeta().getRaw();
@@ -543,7 +543,7 @@ public class TikaDocParserTest extends DocParserTestCase {
         // Meta data
         assertThat(doc.getMeta().getAuthor(), is(nullValue()));
         assertThat(doc.getMeta().getDate(), is(nullValue()));
-        assertThat(doc.getMeta().getKeywords(), emptyIterable());
+        assertThat(doc.getMeta().getKeywords(), nullValue());
         assertThat(doc.getMeta().getTitle(), is(nullValue()));
 
         Map<String, String> raw = doc.getMeta().getRaw();