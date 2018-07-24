80c6a406a7483a37db1ce185ff05e9ec5c73bb20
David Pilato <david@pilato.fr>
2018-07-24 19:28:20 +0200

    WIP: Add support for bulk size in bytes with unit
    We want to be able to support a bulk size in number of documents
    but also in size in bytes, kb, mb...
    
    Related to #545

---
+++ framework/src/main/java/fr/pilato/elasticsearch/crawler/fs/framework/ByteSizeValueDeserializer.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to David Pilato (the "Author") under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Author licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package fr.pilato.elasticsearch.crawler.fs.framework;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+
+/**
+ * Jackson Deserializer for ByteSizeValue object
+ */
+public class ByteSizeValueDeserializer extends StdDeserializer<ByteSizeValue> {
+    public ByteSizeValueDeserializer() {
+        super(TimeValue.class);
+    }
+
+    @Override
+    public ByteSizeValue deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        return ByteSizeValue.parseBytesSizeValue(p.getText());
+    }
+}
