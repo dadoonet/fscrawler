9d2bc7a0dc6cdb2892d59da9497ea1a30dcde41f
David Pilato <david@pilato.fr>
2018-08-23 18:53:37 +0200

    Fix travis build

---
+++ travis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$TRAVIS_SECURE_ENV_VARS" = true ]; then
+   mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar
+fi
+if [ "$TRAVIS_PULL_REQUEST" = false ] && [ "$TRAVIS_TAG" = '' ]; then
+  mvn deploy -DskipTests --settings deploy-settings.xml
+fi
+
