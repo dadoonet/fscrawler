13d6cb29ff8523225c1de045e28a14ead0bfb68b
David Pilato <david@pilato.fr>
2019-02-15 14:46:07 +0100

    Index settings should not set the type anymore

--- elasticsearch-client/elasticsearch-client-v7/src/main/java/fr/pilato/elasticsearch/crawler/fs/client/v7/ElasticsearchClientV7.java
+++ elasticsearch-client/elasticsearch-client-v7/src/main/java/fr/pilato/elasticsearch/crawler/fs/client/v7/ElasticsearchClientV7.java
@@ -235,7 +235,11 @@ public class ElasticsearchClientV7 implements ElasticsearchClient {
             if (e.getMessage().contains("resource_already_exists_exception") && !ignoreErrors) {
                 throw new RuntimeException("index already exists");
             }
+            if (!e.getMessage().contains("resource_already_exists_exception")) {
+                throw e;
+            }
         }
+        client.cluster().health(new ClusterHealthRequest(index).waitForYellowStatus(), RequestOptions.DEFAULT);
     }
 
     /**
