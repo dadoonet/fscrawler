a4ab68aa1a7e0b022d01e4857d2b7b1b703d03a1
Jan Høydahl <jan.git@cominvent.com>
2020-09-06 17:53:53 +0200

    Remove unused import

--- core/src/main/java/fr/pilato/elasticsearch/crawler/fs/TikaProcessor.java
+++ core/src/main/java/fr/pilato/elasticsearch/crawler/fs/TikaProcessor.java
@@ -25,7 +25,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.invoke.MethodHandles;
 import java.security.MessageDigest;
 
