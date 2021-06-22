af868d004889cb9aeb67ffc81aff5a1332ab88d4
David Pilato <david@pilato.fr>
2021-06-22 16:03:06 +0200

    Remove Travis

--- travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$TRAVIS_SECURE_ENV_VARS" = true ]; then
-   mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -DskipIntegTests=true
-fi
-if [ "$TRAVIS_PULL_REQUEST" = false ] && [ "$TRAVIS_TAG" = '' ]; then
-  mvn deploy -DskipTests --settings deploy-settings.xml
-fi
-
