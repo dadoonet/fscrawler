c4eed0f9ecc648a12a6284fb57f6d446b531a8c6
Simon <smndtrl@users.noreply.github.com>
2023-03-01 09:52:30 +0100

    Delete runtest.sh

--- runtest.sh
+++
@@ -1,4 +0,0 @@
-mvn verify -Dtests.class=fr.pilato.elasticsearch.crawler.fs.test.integration.elasticsearch.FsCrawlerTestJsonTransformIT \
-    -Dtests.cluster.user=elastic \
-    -Dtests.cluster.pass=MIqe71xtsibbufYCd8VAfSTD \
-    -Dtests.cluster.url=https://my-deployment-a23909.es.europe-west3.gcp.cloud.es.io
