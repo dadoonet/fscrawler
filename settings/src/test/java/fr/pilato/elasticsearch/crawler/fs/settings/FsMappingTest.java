56927254790f1c75c4aab7dd0c5bb26599646432
Benjamin Dauvissat <benjamin.dauvissat@adelean.com>
2023-11-25 16:38:33 +0100

    Using new mapping settings

--- settings/src/test/java/fr/pilato/elasticsearch/crawler/fs/settings/FsMappingTest.java
+++ settings/src/test/java/fr/pilato/elasticsearch/crawler/fs/settings/FsMappingTest.java
@@ -302,6 +302,18 @@ public class FsMappingTest extends AbstractFSCrawlerMetadataTestCase {
                 "          \"filename\": {\n" +
                 "            \"type\": \"keyword\",\n" +
                 "            \"store\": true\n" +
+                "          },\n" +
+                "          \"created\": {\n" +
+                "            \"type\": \"date\",\n" +
+                "            \"format\": \"date_optional_time\"\n" +
+                "          },\n" +
+                "          \"last_modified\": {\n" +
+                "            \"type\": \"date\",\n" +
+                "            \"format\": \"date_optional_time\"\n" +
+                "          },\n" +
+                "          \"last_accessed\": {\n" +
+                "            \"type\": \"date\",\n" +
+                "            \"format\": \"date_optional_time\"\n" +
                 "          }\n" +
                 "        }\n" +
                 "      },\n" +
