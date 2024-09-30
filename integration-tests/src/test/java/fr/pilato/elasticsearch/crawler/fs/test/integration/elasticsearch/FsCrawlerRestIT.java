377ea15cb39c8f01ed1c55f8dc9b799c7572b4fe
David Pilato <david@pilato.fr>
2024-09-30 19:02:39 +0200

    Fix test

--- integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerRestIT.java
+++ integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerRestIT.java
@@ -32,14 +32,11 @@ import fr.pilato.elasticsearch.crawler.fs.rest.UploadResponse;
 import fr.pilato.elasticsearch.crawler.fs.settings.FsSettings;
 import fr.pilato.elasticsearch.crawler.fs.settings.Rest;
 import fr.pilato.elasticsearch.crawler.fs.test.integration.AbstractRestITCase;
-import fr.pilato.elasticsearch.crawler.plugins.FsCrawlerExtensionFsProvider;
-import fr.pilato.elasticsearch.crawler.plugins.fs.http.FsHttpPlugin;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.errors.MinioException;
 import jakarta.ws.rs.core.MediaType;
-import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.Logger;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.file.FileDataBodyPart;
@@ -50,9 +47,7 @@ import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.utility.MountableFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.security.InvalidKeyException;
@@ -538,10 +533,10 @@ public class FsCrawlerRestIT extends AbstractRestITCase {
             assertThat(uploadResponse.getMessage(), nullValue());
 
             // We wait until we have our document
-            response = countTestHelper(new ESSearchRequest().withIndex(getCrawlerName()), 1L, null);
-            assertThat(JsonPath.read(response.getHits().get(0).getSource(), "$.file.filename"), is("robots.txt"));
-            assertThat(JsonPath.read(response.getHits().get(0).getSource(), "$.file.filesize"), greaterThan(100));
-            assertThat(JsonPath.read(response.getHits().get(0).getSource(), "$.content"), containsString("Sitemap"));
+            response = countTestHelper(new ESSearchRequest().withIndex(getCrawlerName()), 2L, null);
+            assertThat(JsonPath.read(response.getHits().get(1).getSource(), "$.file.filename"), is("robots.txt"));
+            assertThat(JsonPath.read(response.getHits().get(1).getSource(), "$.file.filesize"), greaterThan(100));
+            assertThat(JsonPath.read(response.getHits().get(1).getSource(), "$.content"), containsString("Sitemap"));
         }
     }
 }
