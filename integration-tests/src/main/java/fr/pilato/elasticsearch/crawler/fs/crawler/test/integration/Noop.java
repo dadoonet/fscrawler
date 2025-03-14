6efb6a141d42712143b721a7170ec78591124bc9
David Pilato <david@pilato.fr>
2025-03-14 23:22:46 +0100

    Start central-publishing-maven-plugin for each module

---
+++ integration-tests/src/main/java/fr/pilato/elasticsearch/crawler/fs/crawler/test/integration/Noop.java
@@ -0,0 +1,8 @@
+package fr.pilato.elasticsearch.crawler.fs.crawler.test.integration;
+
+/**
+ * Just to bypass the problem with central-publishing-maven-plugin
+ * which can not be skipped yet
+ */
+public class Noop {
+}
