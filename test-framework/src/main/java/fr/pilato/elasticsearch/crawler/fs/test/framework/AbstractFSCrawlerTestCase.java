1d0afd71b42cd973a30cb74eb86c4864fd95b67b
David Pilato <david@pilato.fr>
2025-04-10 16:19:41 +0200

    Add more time to run tests on Github Actions

--- test-framework/src/main/java/fr/pilato/elasticsearch/crawler/fs/test/framework/AbstractFSCrawlerTestCase.java
+++ test-framework/src/main/java/fr/pilato/elasticsearch/crawler/fs/test/framework/AbstractFSCrawlerTestCase.java
@@ -49,8 +49,8 @@ import static com.carrotsearch.randomizedtesting.RandomizedTest.randomTimeZone;
 @Listeners({FSCrawlerReproduceInfoPrinter.class})
 @ThreadLeakScope(ThreadLeakScope.Scope.SUITE)
 @ThreadLeakLingering(linger = 5000) // 5 sec lingering
-@Timeout(millis = AbstractFSCrawlerTestCase.TIMEOUT_MINUTE_AS_MS)
-@TimeoutSuite(millis = 2 * AbstractFSCrawlerTestCase.TIMEOUT_MINUTE_AS_MS)
+@Timeout(millis = 2 * AbstractFSCrawlerTestCase.TIMEOUT_MINUTE_AS_MS)
+@TimeoutSuite(millis = 3 * AbstractFSCrawlerTestCase.TIMEOUT_MINUTE_AS_MS)
 @ThreadLeakFilters(filters = {
         AbstractFSCrawlerTestCase.TestContainerThreadFilter.class,
         AbstractFSCrawlerTestCase.JNACleanerThreadFilter.class
