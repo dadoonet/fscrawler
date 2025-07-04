5b4c9ef85ebe93999357d4df1662cf699808651c
David Pilato <david@pilato.fr>
2025-07-04 14:14:11 +0200

    Increase time to check that all documents have been removed
    Specifically in Serverless, this can take more than one minute

--- integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerTestSubDirsIT.java
+++ integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerTestSubDirsIT.java
@@ -188,7 +188,7 @@ public class FsCrawlerTestSubDirsIT extends AbstractFsCrawlerITCase {
         deleteRecursively(mainDir);
 
         // We expect to have 1 doc now but this could take some time to happen
-        countTestHelper(new ESSearchRequest().withIndex(getCrawlerName()), 1L, currentTestResourceDir);
+        countTestHelper(new ESSearchRequest().withIndex(getCrawlerName()), 1L, currentTestResourceDir, MAX_WAIT_FOR_SEARCH_LONG_TESTS);
     }
 
     private void folderHitTester(DocumentContext document, int position, String expectedReal, String expectedVirtual,
