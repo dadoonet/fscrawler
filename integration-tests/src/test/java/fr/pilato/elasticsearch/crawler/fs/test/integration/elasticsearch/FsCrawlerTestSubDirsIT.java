a5828242b88d05fab1a0e4c2532318454a9b2f92
jc <newschapmj1@fastmail.fm>
2025-10-26 17:08:24 +0000

    removed redundant comments

--- integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerTestSubDirsIT.java
+++ integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerTestSubDirsIT.java
@@ -218,16 +218,7 @@ public class FsCrawlerTestSubDirsIT extends AbstractFsCrawlerITCase {
                 .withSize(1000)
                 .withSort("path.virtual"));
         assertThat(response.getTotalHits()).isEqualTo(subdirs + 2);
-
-        /*
-        excluded by JC 2025-10-26, will it now pass?
-        if (OsValidator.WINDOWS) {
-            // On windows the deletion does not work as expected
-            // TODO this needs to be fixed
-            logger.warn("On Windows we don't detect properly the recursive removal of directories. So we skip the validation of this test");
-            return;
-        }
-*/
+        
         // Let's remove the main subdir and wait...
         logger.debug("  --> Removing all dirs from [{}]", mainDir);
         deleteRecursively(mainDir);
