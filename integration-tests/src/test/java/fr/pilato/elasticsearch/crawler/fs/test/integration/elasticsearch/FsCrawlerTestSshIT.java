8fae02bb5424a48b428cb9007a7cc2acabdc2059
David Pilato <david@pilato.fr>
2025-11-14 16:50:35 +0100

    Fix test and repo when checking-out on Windows

--- integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerTestSshIT.java
+++ integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerTestSshIT.java
@@ -20,6 +20,7 @@
 package fr.pilato.elasticsearch.crawler.fs.test.integration.elasticsearch;
 
 import fr.pilato.elasticsearch.crawler.fs.client.ESSearchRequest;
+import fr.pilato.elasticsearch.crawler.fs.framework.OsValidator;
 import fr.pilato.elasticsearch.crawler.fs.framework.TimeValue;
 import fr.pilato.elasticsearch.crawler.fs.client.ESSearchResponse;
 import fr.pilato.elasticsearch.crawler.fs.settings.FsSettings;
@@ -40,6 +41,7 @@ import org.junit.Test;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.security.GeneralSecurityException;
@@ -49,6 +51,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeFalse;
 
 /**
  * Test crawler with SSH
@@ -158,6 +161,16 @@ public class FsCrawlerTestSshIT extends AbstractFsCrawlerITCase {
      */
     @Test
     public void dir_with_space_at_the_end() throws Exception {
+        // We need to do a small hack here and rename the test directory as this could not work on Windows
+        Path dirWithSpace = currentTestResourceDir.resolve("with_space ");
+        try {
+            Files.move(currentTestResourceDir.resolve("with_space"), dirWithSpace);
+        } catch (IOException e) {
+            // If we are running on Windows, just ignore the test
+            logger.debug("Cannot rename directory to have a space at the end on Windows. Ignoring the test.", e);
+            assumeFalse("We can not run this test on Windpws", OsValidator.WINDOWS);
+        }
+
         FsSettings fsSettings = createTestSettings();
         fsSettings.getFs().setUrl("/");
         fsSettings.getFs().setUpdateRate(TimeValue.timeValueMinutes(1));
--- integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerTestWeirdFilenamesIT.java
+++ integration-tests/src/test/java/fr/pilato/elasticsearch/crawler/fs/test/integration/elasticsearch/FsCrawlerTestWeirdFilenamesIT.java
@@ -21,16 +21,25 @@ package fr.pilato.elasticsearch.crawler.fs.test.integration.elasticsearch;
 
 import fr.pilato.elasticsearch.crawler.fs.client.ESSearchRequest;
 import fr.pilato.elasticsearch.crawler.fs.client.ESSearchResponse;
+import fr.pilato.elasticsearch.crawler.fs.framework.OsValidator;
 import fr.pilato.elasticsearch.crawler.fs.settings.FsSettings;
 import fr.pilato.elasticsearch.crawler.fs.test.integration.AbstractFsCrawlerITCase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeFalse;
 
 /**
  * Test weird filenames
  */
 public class FsCrawlerTestWeirdFilenamesIT extends AbstractFsCrawlerITCase {
+    private static final Logger logger = LogManager.getLogger();
 
     /**
      * Test for #1952: <a href="https://github.com/dadoonet/fscrawler/issues/1952">https://github.com/dadoonet/fscrawler/issues/1952</a>:
@@ -38,6 +47,16 @@ public class FsCrawlerTestWeirdFilenamesIT extends AbstractFsCrawlerITCase {
      */
     @Test
     public void dir_with_space_at_the_end() throws Exception {
+        // We need to do a small hack here and rename the test directory as this could not work on Windows
+        Path dirWithSpace = currentTestResourceDir.resolve("with_space ");
+        try {
+            Files.move(currentTestResourceDir.resolve("with_space"), dirWithSpace);
+        } catch (IOException e) {
+            // If we are running on Windows, just ignore the test
+            logger.debug("Cannot rename directory to have a space at the end on Windows. Ignoring the test.", e);
+            assumeFalse("We can not run this test on Windpws", OsValidator.WINDOWS);
+        }
+
         FsSettings fsSettings = createTestSettings();
         crawler = startCrawler(fsSettings);
         ESSearchResponse response = countTestHelper(new ESSearchRequest().withIndex(getCrawlerName()), 3L, null);
