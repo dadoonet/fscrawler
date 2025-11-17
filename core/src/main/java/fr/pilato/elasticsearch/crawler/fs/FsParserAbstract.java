2891a370e0b881df1c92cd3ec5d1a50f08531899
jc <newschapmj1@fastmail.fm>
2025-11-17 09:32:04 +0000

    correction

--- core/src/main/java/fr/pilato/elasticsearch/crawler/fs/FsParserAbstract.java
+++ core/src/main/java/fr/pilato/elasticsearch/crawler/fs/FsParserAbstract.java
@@ -648,7 +648,7 @@ public abstract class FsParserAbstract extends FsParser {
             String fullPath = path.concat(pathSeparator).concat(esfile);  
             // On Windows, we must use / as a separator
             String normalizedPath = fullPath.replace("\\", "/");
-            esDelete(managementService, fsSettings.getElasticsearch().getIndex(), generateIdFromFilename(esfile, path));
+            esDelete(managementService, fsSettings.getElasticsearch().getIndex(), SignTool.sign(path.concat(pathSeparator).concat(esfile)));
             stats.removeFile();
         }
 
