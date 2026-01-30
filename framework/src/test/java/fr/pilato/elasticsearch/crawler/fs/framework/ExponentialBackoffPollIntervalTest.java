0acf2bd4571c64990b7f4ce544ace41151fb9adb
David Pilato <david@pilato.fr>
2026-01-30 22:51:18 +0100

    Remove non needed test method

--- framework/src/test/java/fr/pilato/elasticsearch/crawler/fs/framework/ExponentialBackoffPollIntervalTest.java
+++ framework/src/test/java/fr/pilato/elasticsearch/crawler/fs/framework/ExponentialBackoffPollIntervalTest.java
@@ -1,19 +1,32 @@
+/*
+ * Licensed to David Pilato under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package fr.pilato.elasticsearch.crawler.fs.framework;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.awaitility.core.ConditionTimeoutException;
 import org.awaitility.pollinterval.PollInterval;
 import org.junit.Test;
 
 import java.time.Duration;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
 
 public class ExponentialBackoffPollIntervalTest {
-    private static final Logger logger = LogManager.getLogger();
 
     @Test
     public void testExponentialBackoffPollInterval() {
@@ -44,17 +57,5 @@ public class ExponentialBackoffPollIntervalTest {
         // We stay at the max duration
         duration = pollInterval.next(++pollCount, duration);
         assertThat(duration).hasSeconds(5);
-    }
-
-    @Test
-    public void awaitNothing() {
-        logger.info("Start");
-        try {
-            await()
-                    .timeout(Duration.ofMillis(1001))
-                    .untilTrue(new AtomicBoolean(false));
-        } catch (ConditionTimeoutException ignored) {
-        }
-        logger.info("End");
     }
 }
