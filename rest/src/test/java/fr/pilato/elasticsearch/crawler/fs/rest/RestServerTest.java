0b1962e0222fdb0b2e835a765b2463d2243dcb16
David Pilato <david@pilato.fr>
2026-03-12 19:26:58 +0100

    Add a test for Rest service with underscore in the hostname
    Reproduces the bug described in #474.
    
    ```
    GET / HTTP1.1
    Host: fscrawlerrest
    Connection: close
    
    ```
    
    Works:
    
    ```
    HTTP/1.1 200 OK
    Content-Type: text/plain
    Connection: close
    Content-Length: 2
    
    ok
    ```
    
    Whereas:
    
    ```
    GET / HTTP1.1
    Host: fscrawler_rest
    Connection: close
    
    ```
    
    Fails with:
    
    ```
    HTTP/1.1 500 Internal Server Error
    Content-Type: text/html;charset=ISO-8859-1
    Connection: close
    Content-Length: 2629
    
    <html><head><title>Grizzly 4.0.2</title><style><!--div.header {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#003300;font-size:22px;-moz-border-radius-topleft: 10px;border-top-left-radius: 10px;-moz-border-radius-topright: 10px;border-top-right-radius: 10px;padding-left: 5px}div.body {font-family:Tahoma,Arial,sans-serif;color:black;background-color:#FFFFCC;font-size:16px;padding-top:10px;padding-bottom:10px;padding-left:10px}div.footer {font-family:Tahoma,Arial,sans-serif;color:white;background-color:#666633;font-size:14px;-moz-border-radius-bottomleft: 10px;border-bottom-left-radius: 10px;-moz-border-radius-bottomright: 10px;border-bottom-right-radius: 10px;padding-left: 5px}BODY {font-family:Tahoma,Arial,sans-serif;color:black;background-color:white;}B {font-family:Tahoma,Arial,sans-serif;color:black;}A {color : black;}HR {color : #999966;}--></style> </head><body><div class="header">Internal Server Error</div><div class="body"><b>java.net.URISyntaxException: Illegal character in hostname at index 16: http://fscrawler_rest:57601/</b><pre>     1: org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainer.getBaseUri(GrizzlyHttpContainer.java:447)
         2: org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainer.service(GrizzlyHttpContainer.java:356)
         3: org.glassfish.grizzly.http.server.HttpHandler$1.run(HttpHandler.java:190)
         4: org.glassfish.grizzly.threadpool.AbstractThreadPool$Worker.doWork(AbstractThreadPool.java:535)
         5: org.glassfish.grizzly.threadpool.AbstractThreadPool$Worker.run(AbstractThreadPool.java:515)
         6: java.base/java.lang.Thread.run(Thread.java:1583)
    </pre><b>Root Cause: java.net.URISyntaxException: Illegal character in hostname at index 16: http://fscrawler_rest:57601/</b><pre>     1: java.base/java.net.URI$Parser.fail(URI.java:2995)
         2: java.base/java.net.URI$Parser.parseHostname(URI.java:3547)
         3: java.base/java.net.URI$Parser.parseServer(URI.java:3394)
         4: java.base/java.net.URI$Parser.parseAuthority(URI.java:3304)
         5: java.base/java.net.URI$Parser.parseHierarchical(URI.java:3240)
         6: java.base/java.net.URI$Parser.parse(URI.java:3196)
         7: java.base/java.net.URI.<init>(URI.java:732)
         8: org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainer.getBaseUri(GrizzlyHttpContainer.java:445)
         9: org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpContainer.service(GrizzlyHttpContainer.java:356)
        10: org.glassfish.grizzly.http.server.HttpHandler$1.run(HttpHandler.java:190)
            ... 3 more</pre>Please see the log for more detail.</div><div class="footer">Grizzly 4.0.2</div></body></html>
    ```

---
+++ rest/src/test/java/fr/pilato/elasticsearch/crawler/fs/rest/RestServerTest.java
@@ -0,0 +1,111 @@
+/*
+ * Licensed to David Pilato (the "Author") under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. Author licenses this
+ * file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package fr.pilato.elasticsearch.crawler.fs.rest;
+
+import org.assertj.core.api.Assertions;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.Test;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.net.Socket;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Test for REST server behaviour, including issue #474: hostname with underscore
+ * in the Host header causes URISyntaxException in Jersey/Grizzly.
+ *
+ * @see <a href="https://github.com/dadoonet/fscrawler/issues/474">Issue 474</a>
+ * @see <a href="https://github.com/eclipse-ee4j/jersey/issues/728>Issue 728 in Jersey</a>
+ */
+public class RestServerTest {
+
+    // This test should fail when Jersey is upgraded with a fix for this issue.
+    // Then we could remove this test class.
+
+    /**
+     * Reproduces issue #474: when a client sends a request with a Host header
+     * containing an underscore (e.g. Docker service name "fscrawler_rest"),
+     * Jersey's GrizzlyHttpContainer.getBaseUri() parses the request URI and
+     * throws URISyntaxException because java.net.URI treats underscore as
+     * illegal in hostnames.
+     * <p>
+     * We do not need to bind the server to a hostname with underscore. We
+     * start the server on localhost and send a request with a custom Host
+     * header to trigger the same code path.
+     */
+    @Test
+    public void requestWithHostHeaderContainingUnderscoreReturnsServerError() throws Exception {
+        // Bind to localhost with random port (0 = assign any free port)
+        URI baseUri = URI.create("http://127.0.0.1:0");
+        ResourceConfig rc = new ResourceConfig()
+                .register(new RootResource());
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, rc);
+        try {
+            int port = server.getListeners().iterator().next().getPort();
+            String hostWithUnderscore = "fscrawler_rest:" + port;
+            String hostWithoutUnderscore = "fscrawlerrest:" + port;
+
+            // java.net.http.HttpClient does not allow setting the Host header (restricted).
+            // Use a raw socket so we can send Host: fscrawler_rest:port and trigger the bug.
+            String request = "GET / HTTP/1.1\r\nHost: " + hostWithUnderscore + "\r\nConnection: close\r\n\r\n";
+            String response;
+            try (Socket socket = new Socket("127.0.0.1", port)) {
+                socket.getOutputStream().write(request.getBytes(StandardCharsets.US_ASCII));
+                response = new String(socket.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            }
+
+            Assertions.assertThat(response)
+                    .startsWith("HTTP/1.1 500")
+                    .contains("URISyntaxException")
+                    .contains("Illegal character in hostname");
+
+            // java.net.http.HttpClient does not allow setting the Host header (restricted).
+            // Use a raw socket so we can send Host: fscrawler_rest:port and trigger the bug.
+            String request2 = "GET / HTTP/1.1\r\nHost: " + hostWithoutUnderscore + "\r\nConnection: close\r\n\r\n";
+            String response2;
+            try (Socket socket = new Socket("127.0.0.1", port)) {
+                socket.getOutputStream().write(request2.getBytes(StandardCharsets.US_ASCII));
+                response2 = new String(socket.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+            }
+
+            Assertions.assertThat(response2)
+                    .startsWith("HTTP/1.1 200 OK")
+                    .contains("ok");
+        } finally {
+            server.shutdownNow();
+        }
+    }
+
+    @Path("/")
+    public static class RootResource {
+        @GET
+        @Produces(MediaType.TEXT_PLAIN)
+        public String get() {
+            return "ok";
+        }
+    }
+}
