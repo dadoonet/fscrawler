baabe7d79d543f6fb7adc60487bc1f279711140b
David Pilato <david@pilato.fr>
2026-07-21 15:54:46 +0200

    refactor(framework): ♻️ remove unused constructors from Percentage and TimeValue classes

--- framework/src/main/java/fr/pilato/elasticsearch/crawler/fs/framework/Percentage.java
+++ framework/src/main/java/fr/pilato/elasticsearch/crawler/fs/framework/Percentage.java
@@ -18,24 +18,7 @@
  *
  * Made from 🇫🇷🇪🇺 with ❤️ - 2011-2026
  */
-package fr.pilato.elasticsearch.crawler.fs.framework; /*
-                                                       * Licensed to David Pilato (the "Author") under one
-                                                       * or more contributor license agreements.  See the NOTICE file
-                                                       * distributed with this work for additional information
-                                                       * regarding copyright ownership. Author licenses this
-                                                       * file to you under the Apache License, Version 2.0 (the
-                                                       * "License"); you may not use this file except in compliance
-                                                       * with the License.  You may obtain a copy of the License at
-                                                       *
-                                                       *    http://www.apache.org/licenses/LICENSE-2.0
-                                                       *
-                                                       * Unless required by applicable law or agreed to in writing,
-                                                       * software distributed under the License is distributed on an
-                                                       * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-                                                       * KIND, either express or implied.  See the License for the
-                                                       * specific language governing permissions and limitations
-                                                       * under the License.
-                                                       */
+package fr.pilato.elasticsearch.crawler.fs.framework;
 
 import java.util.Locale;
 
@@ -44,12 +27,6 @@ public class Percentage {
     private double value;
 
     private boolean isPercentage;
-
-    public Percentage(String value) {
-        Percentage parsed = parse(value);
-        this.value = parsed.value;
-        this.isPercentage = parsed.isPercentage;
-    }
 
     public Percentage() {
         value = 0;
