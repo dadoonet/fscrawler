ad70ea700644ab7716baebc078795eb604e2ad65
David Pilato <david@pilato.fr>
2026-07-21 16:21:42 +0200

    Revert "refactor(framework): ♻️ remove unused constructors from Percentage and TimeValue classes"
    This reverts commit baabe7d79d543f6fb7adc60487bc1f279711140b.

--- framework/src/main/java/fr/pilato/elasticsearch/crawler/fs/framework/TimeValue.java
+++ framework/src/main/java/fr/pilato/elasticsearch/crawler/fs/framework/TimeValue.java
@@ -18,7 +18,24 @@
  *
  * Made from 🇫🇷🇪🇺 with ❤️ - 2011-2026
  */
-package fr.pilato.elasticsearch.crawler.fs.framework;
+package fr.pilato.elasticsearch.crawler.fs.framework; /*
+                                                       * Licensed to David Pilato (the "Author") under one
+                                                       * or more contributor license agreements.  See the NOTICE file
+                                                       * distributed with this work for additional information
+                                                       * regarding copyright ownership. Author licenses this
+                                                       * file to you under the Apache License, Version 2.0 (the
+                                                       * "License"); you may not use this file except in compliance
+                                                       * with the License.  You may obtain a copy of the License at
+                                                       *
+                                                       *    http://www.apache.org/licenses/LICENSE-2.0
+                                                       *
+                                                       * Unless required by applicable law or agreed to in writing,
+                                                       * software distributed under the License is distributed on an
+                                                       * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+                                                       * KIND, either express or implied.  See the License for the
+                                                       * specific language governing permissions and limitations
+                                                       * under the License.
+                                                       */
 
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
@@ -51,6 +68,12 @@ public class TimeValue {
     private TimeUnit timeUnit;
 
     public TimeValue() {}
+
+    public TimeValue(String time) {
+        TimeValue timeValue = parseTimeValue(time);
+        this.duration = timeValue.duration;
+        this.timeUnit = timeValue.timeUnit;
+    }
 
     public TimeValue(long millis) {
         this(millis, TimeUnit.MILLISECONDS);
